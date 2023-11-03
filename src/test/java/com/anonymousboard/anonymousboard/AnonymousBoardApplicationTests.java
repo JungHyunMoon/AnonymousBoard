e2405cf6782954d6b97e720c073151a19de715c5
문정현 <foqlzm12345@gmail.com>
2023-11-03 11:19:21 +0900

    INIT : 프로젝트 생성

---
+++ src/test/java/com/anonymousboard/anonymousboard/AnonymousBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anonymousboard.anonymousboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnonymousBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

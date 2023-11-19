38e9b123718f5947af064cbc47680b7622ca6900
문정현 <foqlzm12345@gmail.com>
2023-11-20 01:37:24 +0900

    chore : 중복되는 메서드 분리

--- src/main/java/com/anonymousboard/service/BoardService.java
+++ src/main/java/com/anonymousboard/service/BoardService.java
@@ -25,7 +25,6 @@ public class BoardService {
     private final BoardRepository boardRepository;
     private final JwtUtil jwtUtil;
 
-    // 게시글 작성
     @Transactional
     public void saveBoard(Board board, HttpServletRequest req) throws CustomException {
         String rawToken = jwtUtil.getTokenFromRequest(req);
@@ -41,7 +40,6 @@ public class BoardService {
         boardRepository.save(board);
     }
 
-    // 게시글 목록 조회
     public BoardResponseDtos getBoards() {
         BoardResponseDtos boardResponseDtos = new BoardResponseDtos();
         List<Board> boards = boardRepository.findAllByOrderByCreatedAtDesc();
@@ -50,27 +48,25 @@ public class BoardService {
             BoardResponseDto boardResponseDto = new BoardResponseDto(board);
             String username = board.getUsername();
 
-            // 해당 사용자의 리스트가 이미 있는지 확인하고 없으면 새로 생성
             List<BoardResponseDto> userBoards = boardResponseDtos.getBoardResponseDtos().computeIfAbsent(username, k -> new ArrayList<>());
-
-            // 해당 사용자의 게시글을 리스트에 추가
             userBoards.add(boardResponseDto);
         }
 
         return boardResponseDtos;
     }
 
+    public Board getBoardByIdOrThrow(long id) throws CustomException {
+        return boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+    }
 
-    // 게시글 조회
     public BoardResponseDto getBoardById(long id) throws CustomException {
-        Board board = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+        Board board = getBoardByIdOrThrow(id);
         return new BoardResponseDto(board);
     }
 
-    // 게시글 수정
     @Transactional
     public BoardResponseDto updateBoard(long id, UpdateBoardRequestDto requestDto) throws CustomException {
-        Board board = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+        Board board = getBoardByIdOrThrow(id);
         board.update(requestDto);
 
         return new BoardResponseDto(board);
@@ -78,7 +74,7 @@ public class BoardService {
 
     @Transactional
     public void markTaskAsComplete(long id) throws CustomException {
-        Board board = boardRepository.findById(id).orElseThrow(() -> new CustomException(ErrorCode.BOARD_NOT_FOUND));
+        Board board = getBoardByIdOrThrow(id);
         board.setStatus(true);
     }
 

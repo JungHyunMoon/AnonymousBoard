e2f2434bdbd02e595bbedd610487b4f0cf2c6491
문정현 <foqlzm12345@gmail.com>
2023-11-20 02:53:10 +0900

    chore : API 주석 추가

--- src/main/java/com/anonymousboard/controller/BoardController.java
+++ src/main/java/com/anonymousboard/controller/BoardController.java
@@ -7,13 +7,17 @@ import com.anonymousboard.dto.response.BoardResponseDto;
 import com.anonymousboard.entity.Board;
 import com.anonymousboard.exception.CustomException;
 import com.anonymousboard.jwt.JwtUtil;
+import com.anonymousboard.security.UserDetailsImpl;
 import com.anonymousboard.service.BoardService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -22,6 +26,13 @@ public class BoardController {
     private final BoardService boardService;
     private final JwtUtil jwtUtil;
 
+    /**
+     * 할일카드 작성
+     * @param requestDto
+     * @param req
+     * @return
+     * @throws CustomException
+     */
     @PostMapping("/board")
     public ResponseEntity<BoardResponseDto> createBoard(@RequestBody CreateBoardRequestDto requestDto, HttpServletRequest req) throws CustomException {
         Board board = new Board(requestDto);
@@ -29,28 +40,52 @@ public class BoardController {
         return new ResponseEntity<>(new BoardResponseDto(board), HttpStatus.CREATED);
     }
 
+    /**
+     * 할일카드 목록 조회
+     * @return
+     */
     @GetMapping("/board")
     public ResponseEntity<BoardResponseDtos> getBoards() {
         BoardResponseDtos boardResponseDtos = boardService.getBoards();
         return new ResponseEntity<>(boardResponseDtos, HttpStatus.OK);
     }
 
+    /**
+     * 할일카드  조회
+     * @param id
+     * @return
+     * @throws CustomException
+     */
     @GetMapping("/board/{id}")
     public ResponseEntity<BoardResponseDto> getBoardById(@PathVariable long id) throws CustomException {
         BoardResponseDto boardResponseDto = boardService.getBoardById(id);
         return new ResponseEntity<>(boardResponseDto, HttpStatus.OK);
     }
 
+    /**
+     * 할일카드 수정
+     * @param id
+     * @param requestDto
+     * @param req
+     * @return
+     * @throws CustomException
+     */
     @PutMapping("/board")
-    public ResponseEntity<BoardResponseDto> updateBoard(@RequestHeader long id, @RequestBody UpdateBoardRequestDto requestDto, HttpServletRequest req) throws CustomException {
-
-        BoardResponseDto boardResponseDto = boardService.updateBoard(id, requestDto);
+    public ResponseEntity<BoardResponseDto> updateBoard(@RequestHeader long id, @RequestBody UpdateBoardRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) throws CustomException {
+        BoardResponseDto boardResponseDto = boardService.updateBoard(id, requestDto, userDetails);
         return new ResponseEntity<>(boardResponseDto, HttpStatus.OK);
     }
 
+    /**
+     * 할일카드 완료
+     * @param id
+     * @param req
+     * @return
+     * @throws CustomException
+     */
     @PutMapping("/board/complete-status/{id}")
-    public ResponseEntity<String> markTaskAsComplete(@PathVariable long id) throws CustomException {
-        boardService.markTaskAsComplete(id);
+    public ResponseEntity<String> markTaskAsComplete(@PathVariable long id, @AuthenticationPrincipal UserDetailsImpl userDetails) throws CustomException {
+        boardService.markTaskAsComplete(id, userDetails);
         return ResponseEntity.status(HttpStatus.OK).body("상태 변경 완료");
     }
 //
